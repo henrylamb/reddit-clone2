6a7101900874a1105003ec0a6d3c78e0bb738609
henrylamb <henrylamb07@gmail.com>
2021-07-12 18:44:35 +0100

    middleware
    done the features part of the file structure - so data should now be organsied and fetched on request with a an async thunk

--- src/features/feed/feedSlice.js
+++ src/features/feed/feedSlice.js
@@ -1,17 +1,85 @@
-import {createSlice} from 'react-redux'
+import {createSlice} from 'react-redux';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
+
+// have the imports above 
+
+const state = {
+    hasError: false, 
+    isLoading: false,
+    feed: [],
+
+};
 
 export const feedSlice = createSlice({
     name: '',
-    initialState: [],
+    initialState: state,
     reducers: {
+        loadMorePosts: (state, action) => {
+
+            
+        },
 
     },
-    extraReducers: {
-        
-    }
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchData.pending, (state, action) => {
+                state.isLoading = true;
+                state.hasError = false;
+            })
+            .addCase(fetchData.rejected, (state, action) => {
+                state.isLoading = false;
+                state.hasError = true;
+            })
+            .addCase(fetchData.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.hasError = false;
+                state.feed = action.payload;
 
-}
+            })
+        
+
+    }
+    
+
+ }
 
 
 );
+
+// create a thunk so that middleware functions can be made and so that the reddit api can be connected and load 15posts each time a botton is clicked
+
+const fetchData = createAsyncThunk('redditData', async (arg, thunkApi) => {
+    try{
+
+    const response = fetch('https://www.reddit.com/r/popular.json');
+
+    const responseJson = await response.json();
+
+    return responseJson; 
+
+
+    } 
+    catch(error){ 
+        console.log(error)
+    }
+        
+}); 
+
+// creating a usable bit of data for the post: and parts of the feed: 
+
+export const usefulData = {
+    author: state.feed.data.children.author_fullName,
+    title: state.feed.data.children.title,
+    downs: state.feed.data.children.downs,
+    ups: state.feed.data.children.ups,
+    thumbnail: state.feed.data.children.thumbnail,
+}; // not sure if this collection of data from the state actually works - how does useSelector work in this sinarcio
+
+// need to export the correct a relevant parts of the reducers and the state of the things
+
+export const selectFeed = state => state.feedSlice.feed; // the state will be changed with this line - the above useful data should change the original state and thus lead to a rerender to occur
+
+export default feedSlice.reducer;
+
+// plan for future improvement would be to have the argeuments of the fetchData be such that it is the input of the search bar leading to different searches akin to the redditData file
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,12 +1,35 @@
 import { createSlice } from 'react-redux';
+import { usefulData } from '../feed/feedSlice';
+
+const state = {
+    ups: usefulData.ups,
+    downs: usefulData.downs,
+    title: usefulData.title,
+    thumbnail: usefulData.thumbnail,
+    author: usefulData.author,
+}
 
 export const postSlice = createSlice({
-    name: '',
-    initialState: {},
+    name: 'postSlice',
+    initialState: {state},
     reducers: {
-
-    },
-    extraReducers: {
+        upVote: (state, action) => {
+            state.ups += action.payload;
+        },
+        downVote: (state,action) => {
+            state.downs -= action.payload;
+        },
 
     }
+    
 })
+
+export const {upVote, downVote} = postSlice.actions;
+
+export const selectPost = state => state.postSlice.state;
+
+export default postSlice.reducer;
+
+// does the data that will be grabbed by the feedslice need to be imported here as useful data - so that the post can be made
+// or does the redditData file provide the data that is adequate 
+// how can the data be passed down?
